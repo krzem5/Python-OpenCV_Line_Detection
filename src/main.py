ed5edb025f65c9ba672baefcf3760a15c9d2b110
Krzem <krzem5.dev@gmail.com>
2021-04-18 10:40:20 +0200

    Push Update 04/18/2021, 10:40:16

--- src/main.py
+++ src/main.py
@@ -5,26 +5,26 @@ import numpy as np
 
 cap=cv2.VideoCapture(0)
 while True:
-    ret,img=cap.read()
-    gray=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-    try:
-        edges=cv2.Canny(gray,50,150,apertureSize=3)
-        lines=cv2.HoughLines(edges,1,np.pi/180,200)
-        for r,theta in lines[0]:
-            a=np.cos(theta)
-            b=np.sin(theta)
-            x0=a*r
-            y0=b*r
-            x1=int(x0+1000*(-b))
-            y1=int(y0+1000*(a))
-            x2=int(x0-1000*(-b))
-            y2=int(y0-1000*(a))
-            cv2.line(img,(x1,y1),(x2,y2),(0,0,255),2)
-    except IndexError:
-        pass
-    cv2.imshow('img',img)
-    k=cv2.waitKey(30) & 0xff
-    if k==27:break
+	ret,img=cap.read()
+	gray=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+	try:
+		edges=cv2.Canny(gray,50,150,apertureSize=3)
+		lines=cv2.HoughLines(edges,1,np.pi/180,200)
+		for r,theta in lines[0]:
+			a=np.cos(theta)
+			b=np.sin(theta)
+			x0=a*r
+			y0=b*r
+			x1=int(x0+1000*(-b))
+			y1=int(y0+1000*(a))
+			x2=int(x0-1000*(-b))
+			y2=int(y0-1000*(a))
+			cv2.line(img,(x1,y1),(x2,y2),(0,0,255),2)
+	except IndexError:
+		pass
+	cv2.imshow('img',img)
+	k=cv2.waitKey(30) & 0xff
+	if k==27:break
 cap.release()
 cv2.destroyAllWindows()
 img=cv2.imread('image.jpg')
@@ -32,15 +32,15 @@ gray=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 edges=cv2.Canny(gray,50,150,apertureSize=3)
 lines=cv2.HoughLines(edges,1,np.pi/180,200)
 for r,theta in lines[0]:
-    a=np.cos(theta)
-    b=np.sin(theta)
-    x0=a*r
-    y0=b*r
-    x1=int(x0+1000*(-b))
-    y1=int(y0+1000*(a))
-    x2=int(x0-1000*(-b))
-    y2=int(y0-1000*(a))
-    cv2.line(img,(x1,y1),(x2,y2),(0,0,255),2)
+	a=np.cos(theta)
+	b=np.sin(theta)
+	x0=a*r
+	y0=b*r
+	x1=int(x0+1000*(-b))
+	y1=int(y0+1000*(a))
+	x2=int(x0-1000*(-b))
+	y2=int(y0-1000*(a))
+	cv2.line(img,(x1,y1),(x2,y2),(0,0,255),2)
 cv2.imwrite('linesDetected.jpg',img)
 # cap=cv2.VideoCapture(0)
 # _,img=cap.read()
